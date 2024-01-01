57085441a80e814a4b9a6130be8469719a66e665
Yash Chauhan <y4shchauhan@gmail.com>
2024-01-02 01:25:48 +0530

    added sentiment analysis using openai model

--- jobs/socket-streaming.py
+++ jobs/socket-streaming.py
@@ -4,7 +4,7 @@ import time
 import pandas as pd
 
 
-def send_data_over_socket(file_path, host="127.0.0.1", port=9999, chunk_size=2):
+def send_data_over_socket(file_path, host="spark-master", port=9999, chunk_size=2):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(1)
--- jobs/spark-streaming.py
+++ jobs/spark-streaming.py
@@ -1,8 +1,33 @@
 from time import sleep
-from pyspark.sql import SparkSession, Row, Column
+import openai
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, ArrayType
-from pyspark.sql.functions import from_json, explode, col
+from pyspark.sql.functions import from_json, explode, col, udf, when
 from config.config import config
+
+
+def sentiment_analysis(comment) -> str:
+    if comment:
+        openai.api_key = config["openai"]["api_key"]
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": """
+                        You're a machine learning model with a task of classifying comments into POSITIVE, NEGATIVE, NEUTRAL.
+                        You are to respond with one word from the option specified above, do not add anything else.
+                        Here is the comment:
+                        
+                        {comment}
+                    """.format(
+                        comment=comment
+                    ),
+                }
+            ],
+        )
+        return completion.choices[0].message["content"]
+    return "Empty"
 
 
 def start_streaming(spark):
@@ -10,7 +35,7 @@ def start_streaming(spark):
     try:
         stream_df = (
             spark.readStream.format("socket")
-            .option("host", "localhost")
+            .option("host", "0.0.0.0")
             .option("port", 9999)
             .load()
         )
@@ -37,21 +62,30 @@ def start_streaming(spark):
         # Select the individual fields from the struct
         stream_df = stream_df.select("data.*")
 
+        sentiment_analysis_udf = udf(sentiment_analysis, StringType())
+
+        stream_df = stream_df.withColumn(
+            "feedback",
+            when(
+                col("text").isNotNull(), sentiment_analysis_udf(col("text"))
+            ).otherwise(None),
+        )
+
         kafka_df = stream_df.selectExpr(
             "CAST(review_id as STRING) AS key", "to_json(struct(*)) AS value"
         )
 
         query = (
-            kafka_df.writeStream.format("kafka")
-            .option("kafka.bootstrap.servers", config["kafka"]["bootstrap.servers"])
-            .option("kafka.security.protocol", config["kafka"]["security.protocol"])
-            .option("kafka.sasl.mechanism", config["kafka"]["sasl.mechanisms"])
+            kafka_df.writeStream.format("KAFKA")
+            .option("kafka.bootstrap.servers", config["KAFKA"]["bootstrap.servers"])
+            .option("kafka.security.protocol", config["KAFKA"]["security.protocol"])
+            .option("kafka.sasl.mechanism", config["KAFKA"]["sasl.mechanisms"])
             .option(
                 "kafka.sasl.jaas.config",
                 'org.apache.kafka.common.security.plain.PlainLoginModule required username="{username}" '
                 'password="{password}";'.format(
-                    username=config["kafka"]["sasl.username"],
-                    password=config["kafka"]["sasl.password"],
+                    username=config["KAFKA"]["sasl.username"],
+                    password=config["KAFKA"]["sasl.password"],
                 ),
             )
             .option("checkpointLocation", "/tmp/checkpoint")
